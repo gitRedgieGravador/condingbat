fe081a0d353c20ca85c7cb796771735ed698988a
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-27 19:07:18 +0800

    Update bigHeights.java

--- codingbat/java/ap-1/bigHeights.java
+++ codingbat/java/ap-1/bigHeights.java
@@ -1,9 +1,3 @@
-/* We have an array of heights, representing the altitude along a walking 
- * trail. Given start/end indexes into the array, return the number of "big" 
- * steps for a walk starting at the start index and ending at the end index. 
- * We'll say that step is big if it is 5 or more up or down. The start end end 
- * index will both be valid indexes into the array with start <= end.
- */
 public int bigHeights(int[] heights, int start, int end) {
     int count = 0;
     
