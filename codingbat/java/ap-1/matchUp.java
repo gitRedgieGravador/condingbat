4a184bb34e8e329ce068aa416e485c448aab6a5c
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-27 19:10:02 +0800

    Update matchUp.java

--- codingbat/java/ap-1/matchUp.java
+++ codingbat/java/ap-1/matchUp.java
@@ -1,9 +1,3 @@
-/* Given 2 arrays that are the same length containing strings, compare the 
- * 1st string in one array to the 1st string in the other array, the 2nd to 
- * the 2nd and so on. Count the number of times that the 2 strings are 
- * non-empty and start with the same char. The strings may be any length, 
- * including 0.
- */
 public int matchUp(String[] a, String[] b) {
     int count = 0;
     
