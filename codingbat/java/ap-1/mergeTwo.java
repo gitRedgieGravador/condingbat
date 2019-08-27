4e66bcfa31e55a36296cec2c308b03994b38701e
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-27 19:10:21 +0800

    Update mergeTwo.java

--- codingbat/java/ap-1/mergeTwo.java
+++ codingbat/java/ap-1/mergeTwo.java
@@ -1,11 +1,3 @@
-/* Start with two arrays of strings, A and B, each with its elements in 
- * alphabetical order and without duplicates. Return a new array containing 
- * the first N elements from the two arrays. The result array should be in 
- * alphabetical order and without duplicates. A and B will both have a length 
- * which is N or more. The best "linear" solution makes a single pass over A 
- * and B, taking advantage of the fact that they are in alphabetical order, 
- * copying elements directly to the new array.
- */
 public String[] mergeTwo(String[] a, String[] b, int n) {
     String[] arr = new String[n];
     int aIndex = 0;
