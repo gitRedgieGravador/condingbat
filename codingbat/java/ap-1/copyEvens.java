4420cac32b523f743280d9d67183f2edac111b7d
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-27 19:08:55 +0800

    Update copyEvens.java

--- codingbat/java/ap-1/copyEvens.java
+++ codingbat/java/ap-1/copyEvens.java
@@ -1,7 +1,3 @@
-/* Given an array of positive ints, return a new array of length "count" 
- * containing the first even numbers from the original array. The original 
- * array will contain at least "count" even numbers.
- */
 public int[] copyEvens(int[] nums, int count) {
     int[] arr = new int[count];
     int index = 0;
