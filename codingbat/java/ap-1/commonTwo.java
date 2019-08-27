d08383521129da1dc657b393188c2904b033d4a2
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-27 19:08:02 +0800

    Update commonTwo.java

--- codingbat/java/ap-1/commonTwo.java
+++ codingbat/java/ap-1/commonTwo.java
@@ -1,9 +1,3 @@
-/* Start with two arrays of strings, a and b, each in alphabetical order, 
- * possibly with duplicates. Return the count of the number of strings which 
- * appear in both arrays. The best "linear" solution makes a single pass over 
- * both arrays, taking advantage of the fact that they are in alphabetical 
- * order.
- */
 public int commonTwo(String[] a, String[] b) {
     int count = 0;
     int aIndex = 0;
